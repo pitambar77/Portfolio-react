e93c18e4ab378fe623182b3b622a39beaf974cf8
pitambar77 <pitambermajhi33@gmail.com>
2024-02-27 23:38:40 +0530

    upadate

--- src/component/SocialLinks.jsx
+++ src/component/SocialLinks.jsx
@@ -42,7 +42,7 @@ const SocialLinks = () => {
         Resume<BsFillPersonLinesFill size={30}/>
         </>
       ),
-      href:'/resume.pdf',
+      href:'/pitambar resume.pdf',
       style:'rounded-br-md',
       download:true,
     }
